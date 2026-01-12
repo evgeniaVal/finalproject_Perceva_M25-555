e7e29ce0fe2d87c8d5fd846fede759e54576291c
evgeniaVal <perceva221@gmail.com>
2026-01-12 20:27:58 +0300

    add ArgumentParser

--- valutatrade_hub/cli/interface.py
+++ valutatrade_hub/cli/interface.py
@@ -1,30 +1,60 @@
+from argparse import ArgumentParser
 from shlex import split as shlex_split
 
 from prompt import string as prompt_string
 
 
+class MyArgumentParser(ArgumentParser):
+    def error(self, message: str):
+        raise ValueError(message)
+
+
+def build_parser() -> ArgumentParser:
+    parser = MyArgumentParser(prog="valutatrade", add_help=False)
+
+    sub = parser.add_subparsers(dest="command", required=True)
+
+    p_exit = sub.add_parser("exit", aliases=["quit", "q"], add_help=False)
+    p_exit.set_defaults(command="exit")
+
+    p_register = sub.add_parser("register", add_help=False)
+    p_register.add_argument("--username", type=str, required=True)
+    p_register.add_argument("--password", type=str, required=True)
+
+    return parser
+
+
 def get_input(prompt_msg="> "):
     try:
         input_str = prompt_string(prompt_msg).strip()  # type: ignore
-        args = shlex_split(input_str)
+        tokens = shlex_split(input_str) or []
     except (KeyboardInterrupt, EOFError):
-        args = ["exit"]
-    return args[0].lower(), args[1:]
+        tokens = ["exit"]
+    return tokens
 
 
-def process_command(command, args):
-    match command:
-        case "exit" | "quit":
-            print("Выход из ValutaTrade Hub CLI. До свидания!")
+def process_command(parser, tokens):
+    try:
+        ns = parser.parse_args(tokens)
+    except ValueError:
+        print(f"Неизвестная команда: {tokens[0]}")
+        return True
+    match ns.command:
+        case "exit":
+            print("До свидания!")
             return False
+        case "register":
+            print("Parsed args:", ns)
+            return True
         case _:
-            print(f"Неизвестная команда: {command}")
-    return True
+            print(f"Неизвестная команда: {ns.command}")
+            return True
 
 
 def main():
-    print("Приветствие в ValutaTrade Hub CLI!")
-    while process_command(*get_input()):
+    parser = build_parser()
+    print("Приветствие!")
+    while process_command(parser, get_input()):
         pass
 
 
