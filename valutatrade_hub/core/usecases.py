d4fcaf77cff4593a953625f0a61c0fcbf30509ea
evgeniaVal <perceva221@gmail.com>
2026-01-13 13:14:39 +0300

    strip username

--- valutatrade_hub/core/usecases.py
+++ valutatrade_hub/core/usecases.py
@@ -5,11 +5,12 @@ USERS_LOC = "data/users.json"
 
 
 def register(username: str, password: str):
+    uname = username.strip()
     users = load_json(USERS_LOC, default=list)
-    if any(u["username"] == username for u in users):
-        raise ValueError(f"Имя пользователя '{username}' уже занято.")
+    if any(u["username"] == uname for u in users):
+        raise ValueError(f"Имя пользователя '{uname}' уже занято.")
     new_id = max((u["user_id"] for u in users), default=0) + 1
-    new_user = User.from_plain_password(new_id, username, password)
+    new_user = User.from_plain_password(new_id, uname, password)
     users.append(User.to_dict(new_user))
     save_json(USERS_LOC, users)
     return new_id
